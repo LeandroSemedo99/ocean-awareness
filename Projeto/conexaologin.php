ee7a35dc9476f16e0d1caf848181587101b224a3
LeandroSemedo99 <leandrosemedo99@icloud.com>
2025-04-22 15:55:57 +0100

    credentials config

--- Projeto/conexaologin.php
+++ Projeto/conexaologin.php
@@ -1,14 +1,17 @@
 <?php
 
+// Use environment variables to store sensitive information.
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+$host = getenv('DB_HOST');
 
-$user = 'root';
-$password = '';
-$dbname = 'Voluntario';
-$host = 'localhost';
+// Establish a secure connection to the database.
+$mysqli = new mysqli($host, $user, $password, $dbname);
 
-$mysqli = new mysqli($host, $user, $password, $dbname );
-
-if($mysqli->error){
-
-    die("Falha ao conectar á database: " . $mysqli->error);
+// Check for connection errors.
+if ($mysqli->connect_error) {
+    // Log the error to a secure location.
+    error_log("Database connection error: " . $mysqli->connect_error);
+    die("Internal server error. Please try again later.");
 }
